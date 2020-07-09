a6ba0da77167394e1516d8bdb861b9d0a480b0ad
gavinchua <me@gavinchua.com>
2020-07-09 17:29:49 +0800

    remove - orientationchange & resize listener

--- src/js/app.js
+++ src/js/app.js
@@ -9,10 +9,6 @@ import { HeaderFN } from './shared/header';
 import { HomeFN } from './components/home';
 
 (() => {
-  ['orientationchange', 'resize'].forEach((e) => {
-    window.addEventListener(e, () => location.reload(), false);
-  });
-
   if (document.getElementById('btn-menu')) {
     HeaderFN.toggleMenu();
   }
